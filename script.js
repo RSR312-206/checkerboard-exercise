4bb0ad1df10f7451f303af7e80262ff1d7ee1ffe
RSR312-206 <roccosummer@gmail.com>
2015-08-12 20:26:57 -0700

    fixed spacing

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Your JS goes here
 function checker(color) {
-var body = document.querySelector('body');
-var checker = document.createElement('div');
-checker.style.backgroundColor = color;
-checker.style.float = "left";
-checker.style.paddingBottom = '11.1%';
-checker.style.width = '11.1%';
+  var body = document.querySelector('body');
+  var checker = document.createElement('div');
 
-body.appendChild(checker);
+  checker.style.backgroundColor = color;
+  checker.style.float = "left";
+  checker.style.paddingBottom = '11.1%';
+  checker.style.width = '11.1%';
+
+  body.appendChild(checker);
 
 }
 
