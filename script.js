2d0e23af8b7dae44ac904d3692724f2a97a71c6a
RSR312-206 <roccosummer@gmail.com>
2015-08-12 23:32:35 -0700

    flashing one box

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-// Your JS goes here
-function checker(color) {
+
+function checkerBoard() {
   var body = document.querySelector('body');
   var checker = document.createElement('div');
 
-  checker.style.backgroundColor = color;
+  for (var i = 0; i < 63; i++)
+    if (i % 2 === 0) {
+      checker.style.backgroundColor = randomColor();
+    } else if (i % 1 === 0) {
+      checker.style.backgroundColor = randomColor();
+    }
+
   checker.style.float = "left";
   checker.style.paddingBottom = '11.1%';
   checker.style.width = '11.1%';
 
   body.appendChild(checker);
-
 }
 
-for (var i = 0; i <= 62; i++)
-  if (i % 2 === 0) {
-    checker('red');
-  } else if (i % 1 === 0) {
-    checker('black');
+  function randomColor() {
+
+    var colorArray = '0123456789abcdef'.split('');
+    var color = '#';
+
+    for (var i = 0; i < 6; i ++) {
+        color += colorArray[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
+
+  function reset() {
+    document.body.innerHTML = '';
+  }
+
+
+  setInterval(reset, 2000);
+  setInterval(checkerBoard, 2000);
+
+
+
+
+
+
 
